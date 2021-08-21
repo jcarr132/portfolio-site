83bb190af8acc0d49baf841910728ad135c9fc1f
jcarr132 <joshcarr132@gmail.com>
2021-08-21 13:18:04 -0400

    Changes to CV, automate pulling from cv2 repo

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+curl https://raw.githubusercontent.com/jcarr132/cv2/main/public/index.html > ./static/docs/jcarr_cv.html
+curl https://raw.githubusercontent.com/jcarr132/cv2/main/public/jcarr_cv.pdf > ./static/docs/jcarr_cv.pdf
 hugo
